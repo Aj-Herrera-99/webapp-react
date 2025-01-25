19d76d3153b73bd49f34dae4597f595ac5b41d3a
Aj-Herrera-99 <ajherrera999@gmail.com>
2025-01-25 04:56:32 +0100

    movie detail page created

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,17 +1,31 @@
-function MovieCard({movie}) {
-  return (
-      <div key={movie.id} className="bg-blue-300">
-          <div className="flex flex-col">
-              <h3 className="text-xl font-semibold tracking-wider">
-                  {movie.title}
-              </h3>
-              <span>
-                  by <em className="text-lg">{movie.director}</em>
-              </span>
-              <p className="mt-3">{movie.abstract}</p>
-          </div>
-      </div>
-  );
+import { Link } from "react-router-dom";
+
+function MovieCard({ movie }) {
+    return (
+        <div key={movie.id} className="p-3 bg-blue-300">
+            <div className="flex flex-col h-full">
+                <h3 className="text-xl font-semibold tracking-wider">
+                    {movie.title}
+                </h3>
+                <span>
+                    by <em className="text-lg">{movie.director}</em>
+                </span>
+                <p className="my-3">{movie.abstract}</p>
+                <Badge id={movie.id} />
+            </div>
+        </div>
+    );
 }
 
-export default MovieCard
+function Badge({ id }) {
+    return (
+        <Link
+            to={`movies/${id}`}
+            className="px-4 py-1 text-white transition-all scale-95 bg-blue-500 rounded-md w-fit hover:scale-100"
+        >
+            See more
+        </Link>
+    );
+}
+
+export default MovieCard;
--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,5 +1,52 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { movies_url } from "../globals/api";
+
 function MovieDetail() {
-    return <div>MovieDetail</div>;
+    const { id } = useParams();
+    const [movie, setMovie] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        setIsLoading(true);
+        axios
+            .get(`${movies_url}/${id}`)
+            .then((res) => setMovie(res.data))
+            .catch((err) => console.error(err))
+            .finally(() => setIsLoading(false));
+    }, []);
+
+    console.log(movie.reviews);
+    if (isLoading) return <div>Loading...</div>;
+    return (
+        <>
+            <h1 className="text-5xl font-semibold text-blue-500 capitalize">
+                {movie.title}
+            </h1>
+            <h2 className="mt-2 mb-4 text-lg">
+                by <em className="text-xl font-semibold">{movie.director}</em>
+            </h2>
+            <section className="flex flex-col text-lg">
+                <span>Genre: {movie.genre}</span>
+                <span>Release year: {movie.release_year}</span>
+                <span>Overview:</span>
+                <p className="p-4 mx-4 my-1 text-base bg-blue-300 bg-opacity-50 rounded-md">
+                    {movie.abstract}
+                </p>
+            </section>
+            <section>
+                <h2 className="text-lg">Reviews:</h2>
+                {movie.reviews &&
+                    movie.reviews.map((rev) => (
+                        <div className="relative p-4 mx-4 my-1 text-base bg-blue-300 bg-opacity-50 rounded-md">
+                            <p>{rev.text}</p>
+                            <span className="absolute bottom-0 right-3">author: {rev.name}</span>
+                        </div>
+                    ))}
+            </section>
+        </>
+    );
 }
 
 export default MovieDetail;
